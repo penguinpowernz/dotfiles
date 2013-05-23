8fa2dccad1115e4492a7b9a601c44ca88ffce7b5
Robert McLeod <hamstar@telescum.co.nz>
2013-05-24 02:43:39 +1200

    made install.sh work properly

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 echo -n "Install into $HOME? [Y/n]: ";
 read INSTALL;
 
-if [ "$(grep -i y $INSTALL)" = 0 ]; then
+if [ "$INSTALL" = "" ] || [ "$(grep -i y $INSTALL|wc -l)" = "1" ]; then
+  echo "Installing...";
+else
   echo "Not installing.";
   exit 0;
 fi;
@@ -11,4 +13,4 @@ fi;
 THIS=`readlink -e $0`;
 FROM=`dirname $THIS`;
 
-rsync -rav $FROM/* $HOME --exclude=".git,README.md,install.sh";
+rsync -rav $FROM/ $HOME --exclude=".git" --exclude="README.md" --exclude="install.sh";
